d90680b18f3f2fb376a695fad99496e5be20a93d
Elvin Reyes <elvin_reyes01@hotmail.com>
2020-09-14 20:51:48 -0400

    Proyecto final

---
+++ FinalProyect-main/Proyectofinal/AlbumDetailPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace Proyectofinal
+{
+    public partial class AlbumDetailPage : ContentPage
+    {
+        private const string URL = @"https://jsonplaceholder.typicode.com/photos?albumId={0}";
+        private int AlbumId;
+        private HttpClient httpClient = new HttpClient();
+
+        #region Properties
+
+        public ObservableCollection<Photo> Photos { get; set; }
+
+        #endregion Properties
+
+        public AlbumDetailPage(int albumId)
+        {
+            InitializeComponent();
+
+            AlbumId = albumId;
+            Photos = new ObservableCollection<Photo>();
+            BindingContext = this;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            //TODO: Implementar el codigo para descargar la lista de fotos de un album
+            // Usar la constante URL declarada al inicio de la clase y reemplazar {0}
+            // Por la variable albumId
+
+            IsBusy = true;
+
+            var url = string.Format(URL, AlbumId);
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                try
+                {
+                    var items = JsonConvert.DeserializeObject<List<Photo>>(jsonResponse);
+
+                    if (items != null && items.Any())
+                    {
+                        Photos.Clear();
+                        foreach(var item in items)
+                        {
+                            Photos.Add(item);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Error", "No se pudo obtener el Album seleccionado", "Ok");
+                }
+
+            }
+            else
+            {
+                await DisplayAlert("Error", "No se pudo obtener el Album seleccionado", "Ok");
+            }
+
+            photoCollection.ItemsSource = Photos;
+            IsBusy = false;
+        }
+    }
+}
---
+++ FinalProyect-main/Proyectofinal/RegisterPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace Proyectofinal
+{
+    public class RegisterResponse
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+    }
+
+    public partial class RegisterPage : ContentPage
+    {
+        private const string URL = @"https://reqres.in/api/register";
+
+        #region Propiedades
+        //TODO: Crear las propiedades Email y Password para hacer Binding con el XAML
+
+        private string _email;
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+
+            set
+            {
+                _email = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                _password = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public Command Register { get; set; }
+
+        #endregion
+
+        public bool CanLogin { get { return !IsBusy; } }
+
+        private HttpClient client = new HttpClient();
+
+        public RegisterPage()
+        {
+            InitializeComponent();
+
+            Register = new Command(OnRegister);
+            BindingContext = this;
+        }
+
+        private async void OnRegister()
+        {
+            try
+            {
+                IsBusy = true;
+
+                //TODO: Reemplazar los valores con las propiedades Email y Password
+                var credentials = new
+                {
+                    email = Email,
+                    password = Password
+                };
+
+                string json = JsonConvert.SerializeObject(credentials);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await this.client.PostAsync(URL, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                    var loginResponse = JsonConvert.DeserializeObject<RegisterResponse>(jsonResponse);
+
+                    if (!string.IsNullOrEmpty(loginResponse.Token))
+                    {
+                        await Navigation.PushModalAsync(new MainTabbedPage());
+                    }
+                }
+                else
+                {
+                    var x = await response.Content.ReadAsStringAsync();
+                    await DisplayAlert("Error", x, "Ok");
+                }
+
+                IsBusy = false;
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Error", "Error procesando...", "Ok");
+                IsBusy = false;
+            }
+        }
+    }
+}
